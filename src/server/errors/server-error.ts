26eaae3ef22bae7174488a4d72412b0758c3f950
Roberto Ríos <roberto@rrios.dev>
2025-08-13 19:48:49 +0200

    refactor: enhance ServerError class to support generic metadata; update constructor and interface for improved type safety

--- src/server/errors/server-error.ts
+++ src/server/errors/server-error.ts
@@ -1,23 +1,28 @@
 import { StatusCodes } from 'http-status-codes';
 
-interface ServerErrorProps {
+interface ServerErrorProps<Meta extends Record<string, unknown>> {
   message: string;
   uiMessage?: string;
   code?: string;
   status?: StatusCodes;
+  meta?: Meta;
 }
 
-export default class ServerError extends Error {
+export default class ServerError<
+  Meta extends Record<string, unknown> = Record<string, unknown>
+> extends Error {
   code?: string;
   status: StatusCodes;
   uiMessage?: string;
+  meta?: Meta;
 
-  constructor(props: ServerErrorProps) {
+  constructor(props: ServerErrorProps<Meta>) {
     super(props.message);
     this.name = 'ServerError';
     this.code = props.code ?? 'GENERIC_ERROR';
     this.status = props.status ?? StatusCodes.INTERNAL_SERVER_ERROR;
     this.uiMessage = props.uiMessage;
+    this.meta = props.meta;
   }
 
   toJSON() {
